46e0a0fcfe98719038a1ee57f88d5bb177b22303
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-01-31 14:43:03 -0600

    Add UIPanGestureRecognizer

--- Transitions/RedViewController.swift
+++ Transitions/RedViewController.swift
@@ -17,15 +17,32 @@ class RedViewController: LabelProvidingVC, UIViewControllerTransitioningDelegate
         dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func pan(_ sender: Any) {
-        
+    func interactionControllerForDismissal(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        // Interaction controller is going to control the transition
+        return interactionController
     }
     
-    // MARK: - Super class lifecycle
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func pan(_ sender: UIPanGestureRecognizer) {
         
+        switch sender.state {
+        case .began:
+            interactionController = UIPercentDrivenInteractiveTransition()
+            dismiss(sender)
+        case .changed:
+            let translation = sender.translation(in: view)
+            let percentage = abs(translation.y / view.bounds.height)
+            interactionController?.update(percentage)
+        case .ended:
+            let velocity = sender.velocity(in: view).y
+            
+            if velocity > 100 {
+                interactionController?.finish()
+            } else {
+                interactionController?.cancel()
+            }
+        default:
+            break
+        }
     }
     
     func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
@@ -33,4 +50,5 @@ class RedViewController: LabelProvidingVC, UIViewControllerTransitioningDelegate
     }
     
     let animator = Animator()
+    private var interactionController: UIPercentDrivenInteractiveTransition?
 }
